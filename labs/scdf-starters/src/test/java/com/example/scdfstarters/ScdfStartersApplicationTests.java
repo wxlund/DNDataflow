7eba143d43d605581496e2da279afea184f991fe
wxlund <wxlund@yahoo.com>
2019-03-05 14:50:04 -0800

    first changes to lab1:

---
+++ labs/scdf-starters/src/test/java/com/example/scdfstarters/ScdfStartersApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.scdfstarters;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScdfStartersApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
