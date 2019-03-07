281e3ea5ffb0ca009f0e88f9a43d703c3bbfb972
wxlund <wxlund@yahoo.com>
2019-03-07 13:16:41 -0800

    finished lab1 & lab2, removed old jars and updated to new jars, updated the poms and code from lab1 & lab2

--- labs/lab2/src/main/java/io/spring/TasklabApplication.java
+++ labs/lab2/src/main/java/io/spring/TasklabApplication.java
@@ -3,14 +3,18 @@ package io.spring;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.task.configuration.EnableTask;
+import org.springframework.cloud.task.listener.annotation.AfterTask;
+import org.springframework.cloud.task.listener.annotation.BeforeTask;
+import org.springframework.cloud.task.repository.TaskExecution;
 //import org.springframework.cloud.task.configuration.EnableTask;
 //import org.springframework.cloud.task.listener.annotation.AfterTask;
 //import org.springframework.cloud.task.listener.annotation.BeforeTask;
 //import org.springframework.cloud.task.repository.TaskExecution;
 import org.springframework.context.annotation.Bean;
 
+@EnableTask
 @SpringBootApplication
-
 public class TasklabApplication {
 
 	public static void main(String[] args) {
@@ -22,10 +26,21 @@ public class TasklabApplication {
 		return new CommandLineRunner() {
 			@Override
 			public void run(String... strings) throws Exception {
+//				throw new IllegalStateException("No Task For You!!");
 				System.out.println("Hello World");
 
 			}
 		};
+	}
+
+	@BeforeTask
+	public void beforeTask(TaskExecution taskExecution) {
+		System.out.println("Before TASK");
+	}
+
+	@AfterTask
+	public void afterTask(TaskExecution taskExecution) {
+		System.out.println("After TASK");
 	}
 
 }
